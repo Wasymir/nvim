6d4d01ef4a0e04db911cb14907f1064c54dc1c37
Wasymir <wasymir@gmail.com>
2023-07-19 17:57:03 -0400

    added rpn calculator xd

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,37 +1,39 @@
 return {
-	'hrsh7th/nvim-cmp',
-	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		},
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-	},
-	config = function()
-		local cmp = require 'cmp'
-		cmp.setup {
-			snippet = {
-				expand = function(args)
-					require('luasnip').lsp_expand(args.body)
-				end
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        {
+            "L3MON4D3/LuaSnip",
+            version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        },
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'PhilRunninger/cmp-rpncalc'
+    },
+    config = function()
+        local cmp = require 'cmp'
+        cmp.setup {
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end
 
-			},
-			mapping = cmp.mapping.preset.insert({
-				['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
-				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-			}),
-			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
-				{ name = 'buffer' },
-				{ name = 'path' }
-			})
-		}
-	end
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+                { name = 'path' },
+                { name = 'rpncalc' }
+            })
+        }
+    end
 
 }
